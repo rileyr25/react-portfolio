b3b0be695a451c8431dac46bb910df6213d4a095
Riley Robertson <riley.r@telus.net>
2021-11-24 21:37:40 -0800

    added typist + downgraded npm to v6.14.13

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Typist from 'react-typist'
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <p>This is a test why is no work</p>
+            <h1>Riley Robertson</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste velit amet minus sint, nemo necessitatibus?</p>
+            <p>Front End 
+                <span>
+                &lt; 
+                <Typist>
+                    <span>Developer</span>
+                    <Typist.Backspace count={9} delay={4000}/>
+                    <span>Designer</span>
+                </Typist>
+                /&gt;
+                </span>
+            </p>
         </div>
     )
 }
