4ba27d8799342e251131f41010766c3b9c7ab066
Riley Robertson <riley.r@telus.net>
2021-11-30 21:20:23 -0800

    cleaned up unused code

--- src/components/home/HomeScroll.js
+++ src/components/home/HomeScroll.js
@@ -4,14 +4,14 @@ import {content} from '../../data/content'
 // import { Navigation } from 'swiper';
 // import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/swiper-bundle.min.css'
+// import 'swiper/swiper-bundle.min.css'
 
-// swiper core styles
-import 'swiper/swiper.min.css'
+// // swiper core styles
+// import 'swiper/swiper.min.css'
 
-// modules styles
-import 'swiper/components/navigation/navigation.min.css'
-import 'swiper/components/pagination/pagination.min.css'
+// // modules styles
+// import 'swiper/components/navigation/navigation.min.css'
+// import 'swiper/components/pagination/pagination.min.css'
 
 function HomeScroll() {
     console.log(content)
@@ -26,7 +26,7 @@ function HomeScroll() {
                 <h1>{proData.title}</h1>
                 <p>{proData.excerpt}</p>
 
-                <Swiper
+                {/* <Swiper
                     modules={[Navigation]}
                     pagination={{ clickable: true }}
                     scrollbar={{ draggable: true }}
@@ -36,7 +36,7 @@ function HomeScroll() {
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img1}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img2}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img3}`} alt="" /></SwiperSlide>
-                </Swiper>
+                </Swiper> */}
             </section>
 
             <section className="home-lab">
