120d037e2a9250c2b0cc388597888917d8c73301
Riley Robertson <riley.r@telus.net>
2021-12-16 12:33:48 -0800

    changed slider structure

--- src/components/home/HomeScroll.js
+++ src/components/home/HomeScroll.js
@@ -10,8 +10,8 @@ import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
 import 'swiper/swiper.scss'; // core Swiper
 import 'swiper/modules/navigation/navigation.scss'; // Navigation module
 import 'swiper/modules/pagination/pagination.scss'; // Pagination module
-import 'swiper/modules/effect-coverflow/effect-coverflow.scss';
-import 'swiper/css/effect-coverflow';
+// import 'swiper/modules/effect-coverflow/effect-coverflow.scss';
+// import 'swiper/css/effect-coverflow';
 
 // // import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper';
 // import { Navigation } from 'swiper';
@@ -38,18 +38,19 @@ function HomeScroll() {
             <section className="home-projects">
                 <h1>{proData.title}</h1>
                 <p>{proData.excerpt}</p>
-
-                <Swiper
-                    modules={[Navigation, Pagination, EffectCoverflow]}
-                    pagination={{ clickable: true }}
-                    scrollbar={{ draggable: true }}
-                    onSlideChange={() => console.log("slide change")}
-                    onSwiper={swiper => console.log(swiper)}
-                    >
-                    <SwiperSlide><img src={`${IMG_PATH}${proData.images.img1}`} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={`${IMG_PATH}${proData.images.img2}`} alt="" /></SwiperSlide>
-                    <SwiperSlide><img src={`${IMG_PATH}${proData.images.img3}`} alt="" /></SwiperSlide>
-                </Swiper>
+                <div className="swiper-container">
+                    <Swiper
+                        modules={[Navigation, Pagination, EffectCoverflow]}
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                        onSlideChange={() => console.log("slide change")}
+                        onSwiper={swiper => console.log(swiper)}
+                        >
+                        <SwiperSlide><img src={`${IMG_PATH}${proData.images.img1}`} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={`${IMG_PATH}${proData.images.img2}`} alt="" /></SwiperSlide>
+                        <SwiperSlide><img src={`${IMG_PATH}${proData.images.img3}`} alt="" /></SwiperSlide>
+                    </Swiper>
+                </div>
             </section>
 
             <section className="home-lab">
