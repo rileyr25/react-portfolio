7dea13ad3444ed2ff49cf0c3905f05b70ce72619
Riley Robertson <riley.r@telus.net>
2021-12-18 19:47:11 -0800

    added project filter

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,12 +3,12 @@ import { content } from '../../data/content'
 function Projects() {
 
     let siteData = content.projects.sites
-
+    let siteFilter = ''
     return (
         <div>
             <h1>Projects</h1>
             
-            {siteData.map(site => 
+            {siteData.filter(site => site.type === {siteFilter}).map(site => 
                 <div className="site-card">
                     <h3>{site.title}</h3>
                     <p>{site.description}</p>
