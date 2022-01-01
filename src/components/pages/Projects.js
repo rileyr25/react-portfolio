4382440be54286b97efb49c7f1b29214ea8bf4a6
Riley Robertson <riley.r@telus.net>
2021-12-31 16:22:49 -0800

    changed filter button to radio

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,22 +3,37 @@ import { content } from '../../data/content'
 function Projects() {
 
     let siteData = content.projects.sites
-    let siteFilter = ''
+    let siteFilter = 'dev'
+
+
+
 
     console.log(siteData)
 
     // TODO: figure out how to access object property 'type' + evaluate value
     // create a button that toggles the 'type' value that is displayed
     // rewrite siteData filter
-
     return (
         <div>
             <h1>Projects</h1>
-            
-            <button onClick={siteFilter = 'dev'}>Filter</button>
+            <form>
+                <input 
+                type="radio"
+                id='dev'
+                name='project-type'
+                value='dev'
+                />
+            </form>
+
+            {/* {siteData.filter(site => site.type === 'dev').map(site => 
+                <div className="site-card">
+                    <h3>{site.title}</h3>
+                    <p>{site.description}</p>
+                </div>
+                )} */}
 
 
-            {siteData.filter(site => site.type === {siteFilter}).map(site => 
+            {siteData.filter(site => site.type === siteFilter).map(site => 
                 <div className="site-card">
                     <h3>{site.title}</h3>
                     <p>{site.description}</p>
