b9604060208f67e52786768af117da44812ab6db
Riley Robertson <riley.r@telus.net>
2022-01-02 16:15:54 -0800

    finish type toggle & add react-icons

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { content } from '../../data/content'
 function Projects() {
 
     let siteData = content.projects.sites
-    let siteFilter = 'dev'
-
-
-
-
-    console.log(siteData)
+    const [type, setType] = useState('dev')
+    
+    const toggleType = e => {
+        setType(e.target.value)
+    }
 
     // TODO: figure out how to access object property 'type' + evaluate value
     // create a button that toggles the 'type' value that is displayed
@@ -16,29 +15,38 @@ function Projects() {
     return (
         <div>
             <h1>Projects</h1>
-            <form>
+            <div className='type-toggle'>
                 <input 
                 type="radio"
                 id='dev'
                 name='project-type'
                 value='dev'
+                defaultChecked
+                onClick={toggleType}
                 />
-            </form>
-
-            {/* {siteData.filter(site => site.type === 'dev').map(site => 
-                <div className="site-card">
-                    <h3>{site.title}</h3>
-                    <p>{site.description}</p>
-                </div>
-                )} */}
-
-
-            {siteData.filter(site => site.type === siteFilter).map(site => 
-                <div className="site-card">
-                    <h3>{site.title}</h3>
-                    <p>{site.description}</p>
-                </div>
+                <input 
+                type="radio"
+                id='design'
+                name='project-type'
+                value='design'
+                onClick={toggleType}
+                />
+            </div>
+            <div className='site-display'>
+                {siteData.filter(site => site.type === type).map(site => 
+                    <div className="site-card">
+                        <h3>{site.title}</h3>
+                        <p>{site.description}</p>
+                            <div className="languages">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                    </div>
                 )}
+            </div>
         </div> 
     )
 }
