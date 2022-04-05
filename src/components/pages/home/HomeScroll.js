7281507864aa3695e2a81ae456996683eceefa86
Riley Robertson <riley.r@telus.net>
2022-04-04 23:34:10 -0700

    swiper config

--- src/components/pages/home/HomeScroll.js
+++ src/components/pages/home/HomeScroll.js
@@ -2,8 +2,9 @@ import React from 'react'
 import {content} from '../../../data/content'
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, EffectCards, EffectCoverflow } from "swiper";
+import { Autoplay, EffectCards, EffectCoverflow, Navigation } from "swiper";
 import "swiper/css/effect-cards";
+import "swiper/css/navigation";
 import 'swiper/css'
 
 
@@ -14,31 +15,34 @@ function HomeScroll() {
     const IMG_PATH = '/assets/images/'
 
     return (
-        <section className='home-scroll pt-12'>
-            <section id='projects' className="home-projects">
-                <h2 className='text-center font-semibold text-2xl'>{proData.title}</h2>
-                <p>{proData.excerpt}</p>
+        <section className='home-scroll pt-12 px-6 max-w-45rem m-auto'>
+            <section id='projects' className="home-projects text-center pb-8">
+                <h2 className='font-semibold text-2xl p-5'>{proData.title}</h2>
+                <p className=''>{proData.excerpt}</p>
                 <Swiper
+                slidesPerView={1}
+                loop={true}
+                grabCursor={true}
                 effect={"cards"}
                 autoplay={{
                     delay: 4000,
                     disableOnInteraction: false,
                 }}
-                grabCursor={true}
                 modules={[EffectCards, Autoplay]}
                 className="project-images">
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img1}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img2}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img src={`${IMG_PATH}${proData.images.img3}`} alt="" /></SwiperSlide>
                 </Swiper>
-                <Link to='/projects'>View Projects</Link>
+                <Link to='/projects' className='w-full '>View Projects</Link>
             </section>
 
-            <section id='lab' className="home-lab">
-                <h2 className='text-center font-semibold text-2xl'>{labData.title}</h2>
-                <p>{labData.excerpt}</p>
+            <section id='lab' className="home-lab text-center pb-8">
+                <h2 className='font-semibold text-2xl p-5'>{labData.title}</h2>
+                <p className=''>{labData.excerpt}</p>
                 <Swiper
                 effect={"coverflow"}
+                loop={true}
                 coverflowEffect={{
                     rotate: 50,
                     stretch: 0,
@@ -46,13 +50,15 @@ function HomeScroll() {
                     modifier: 1,
                     slideShadows: true,
                 }}
-                modules={[EffectCoverflow]}
+                grabCursor={true}
+                navigation={true}
+                modules={[EffectCoverflow, Navigation]}
                 className="lab-images">
                     <SwiperSlide><img  src={`${IMG_PATH}${labData.images.img1}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img  src={`${IMG_PATH}${labData.images.img2}`} alt="" /></SwiperSlide>
                     <SwiperSlide><img  src={`${IMG_PATH}${labData.images.img3}`} alt="" /></SwiperSlide>
                 </Swiper>
-                <Link to='/lab'>Visit Lab</Link>
+                <Link to='/lab' className='w-full text-center'>Visit Lab</Link>
             </section> 
         </section>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       maxWidth: {
         '5/6': '83%',
+        '45rem': '45rem'
       },
     },
   },
