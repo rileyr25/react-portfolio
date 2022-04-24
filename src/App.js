2bec6977022ad60ba5c32964d7550dcec2538072
Riley Robertson <riley.r@telus.net>
2022-04-24 12:52:35 -0700

    added scroll to top wrapper

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { useLayoutEffect } from 'react';
 import NavBar from './components/global/NavBar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
 import Lab from './components/pages/Lab';
 import Single from './components/pages/Single';
 import Projects from './components/pages/Projects';
-import useDarkMode from './hooks/useDarkMode';
-import Toggle from './components/global/Toggle';
+
+const Wrapper = ({children}) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+} 
 
 function App() {
   return (
-    <div id='#' className="app bg-slate-200 dark:bg-darkbg dark:text-white mb-14 md:mb-0">
+    <Wrapper>
+    <div id='#' className="app bg-slate-200 dark:bg-darkbg dark:text-white pb-14 md:pb-0">
       <NavBar />
       <Routes>
         <Route path="/" exact element={<Home />}/>
@@ -20,6 +28,7 @@ function App() {
         <Route path='about' element={<About />}/>
       </Routes>
     </div>
+    </Wrapper>
   );
 }
 
